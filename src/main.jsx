d948071509c7a84b4e55ce7a00b6177ae26d09de
Mtslima123 <mtslima123@gmail.com>
2026-05-21 13:49:13 -0300

    Delete src directory

--- src/main.jsx
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import EstimativaApp from './App.jsx'
-createRoot(document.getElementById('root')).render(<EstimativaApp />)
