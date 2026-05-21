6c4b673207aac659d57f9bcb1eb6b5d5209eb3c8
Mtslima123 <mtslima123@gmail.com>
2026-05-21 13:38:35 -0300

    Att vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-export default defineConfig({
-  plugins: [react()],
-  base: '/',
-  build: { outDir: 'dist', assetsDir: 'assets', chunkSizeWarningLimit: 3000 },
-})
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: { extend: {} },
+  plugins: [],
+}
