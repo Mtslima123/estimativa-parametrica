155379b5a2a3268060b8670fc14441cdfbf165bf
Mtslima123 <mtslima123@gmail.com>
2026-05-21 00:06:07 -0300

    Painel

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import EstimativaApp from './estimativa_app.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <EstimativaApp />
+  </React.StrictMode>
+)
