4917d8f8f5b9f73056f5385e0dd129b7e49fcbcd
Mtslima123 <mtslima123@gmail.com>
2026-05-21 00:32:40 -0300

    sssssss

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import HomeApp from './HomeApp.jsx'
+import EstimativaApp from './estimativa_app.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HomeApp />
+    <EstimativaApp />
   </React.StrictMode>
 )
